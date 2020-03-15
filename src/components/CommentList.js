968488d33c9b62ea2d908ba55dcced39a1d56fd1
Harsh Kedia <harshkedia@outlook.in>
2020-03-15 15:21:11 +0100

    Basic Testing

---
+++ src/components/CommentList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class CommentList extends React.Component {
+  renderComments = () => {
+    return this.props.comments.map((comment, index) => {
+      return <li key={ index }>{ comment }</li>;
+    });
+  };
+
+  render () {
+    return (
+      <div>
+        <ul>
+          { this.renderComments() }
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    comments: state.comments
+  };
+}
+
+export default connect(mapStateToProps)(CommentList);
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Root from 'Root';
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <CommentBox />
+    </Root>
+  );
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has a textarea and a button', () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
+
+describe('the textarea', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  });
+
+  it('has a textarea that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('clears the textarea when form is submitted', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+
+});
