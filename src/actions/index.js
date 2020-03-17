7a6be65bf16bc42a2a61fe5925d55fda19ebd9d2
Harsh Kedia <harshkedia@outlook.in>
2020-03-17 13:23:38 +0100

    middleware boilerplate

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 
-export function saveComment (comment) {
+export const saveComment = comment => {
   return {
     type: SAVE_COMMENT,
     payload: comment
   };
-}
+};
 
-export async function fetchComments () {
+export const fetchComments = async () => {
   const response = await axios.get('http://jsonplaceholder.typicode.com/comments');
   return {
     type: FETCH_COMMENTS,
     payload: response.data
   };
-}
+};
 
-export function changeAuth (isLoggedIn) {
+export const changeAuth = isLoggedIn => {
   return {
     type: CHANGE_AUTH,
     payload: isLoggedIn
   };
-}
+};
