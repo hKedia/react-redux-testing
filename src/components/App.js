16fb0e2153289b9447989dc849db83dc78c6e18e
Harsh Kedia <harshkedia@outlook.in>
2020-03-17 11:58:11 +0100

    preparation for creating a hoc

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentList from 'components/CommentList';
 import CommentBox from 'components/CommentBox';
+import { changeAuth } from 'actions';
 
-const App = () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
+class App extends React.Component {
+  renderButton () {
+    if (this.props.auth) {
+      return (
+        <button onClick={ () => this.props.changeAuth(false) }>Sign Out</button>
+      );
+    } else {
+      return (
+        <button onClick={ () => this.props.changeAuth(true) }>Sign In</button>
+      );
+    }
+  }
+
+  renderHeader () {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>
+          { this.renderButton() }
+        </li>
+      </ul>
+    );
+  }
+
+  render () {
+    return (
+      <div>
+        { this.renderHeader() }
+        <Route path="/post" component={ CommentBox } />
+        <Route path="/" exact component={ CommentList } />
+      </div>
+    );
+  }
 };
 
-export default App;
+function mapStateToProps (state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps, { changeAuth })(App);
