ae80a5aa2ac457c02b98a6a996bbeddf0db7594e
Harsh Kedia <harshkedia@outlook.in>
2020-03-17 13:35:50 +0100

    middleware for async calls

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,3 +1,16 @@
 export default ({ dispatch }) => next => action => {
+  // check to see if the action has a promise on 'payload'
+  // If it does, wait for it to resolve
+  // Else send the action onto the next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+
+  // Wait for promise to resolve, get the data
+  // create a new action and dispatch
+  action.payload.then((response) => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
 
 };
